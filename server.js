f7705c54884aa7813781142a62242ae1b3a223b3
yijkv <bogdannutiu25@gmail.com>
2023-11-18 13:47:02 +0200

    Fixed bug

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express()
 app.use(express.static("public"))
 app.set("view engine","ejs")
 app.get("/user", (req,res) =>{
-    app.render("Hello")
+    res.render("Hello")
 })
 app.listen(3000)
