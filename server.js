369272b7e8b35b13446e436b71d258d55418a1c5
yijkv <bogdannutiu25@gmail.com>
2023-11-18 13:44:16 +0200

    Nutiu commit

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require("express")
 const app = express()
 app.use(express.static("public"))
 app.set("view engine","ejs")
+app.get("/user", (req,res) =>{
+    app.render("Hello")
+})
 app.listen(3000)
